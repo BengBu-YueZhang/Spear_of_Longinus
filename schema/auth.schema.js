57917d520e49f3e4e6ee744b3d3c280ea264b04e
BengBu-YueZhang <zhangyue_coder@163.com>
2018-08-26 09:16:33 +0800

    建立auth和role的模型

--- schema/auth.schema.js
+++ schema/auth.schema.js
@@ -2,6 +2,22 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const AuthSchema = new Schema({
+  // 权限的代码
+  code: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  // 权限的名称
+  name: {
+    type: String,
+    required: true
+  },
+  // 权限的分组
+  group: {
+    type: String,
+    required: true
+  }
 })
 
 module.exports = AuthSchema
--- schema/role.schema.js
+++ schema/role.schema.js
@@ -2,8 +2,30 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 // 加载角色，因为需要加载到redis中，需要注意在redis链接成功的回调中
-// 每一次修改用户信息都需要同步到redis中
+// 每一次修改用户信息都需要同步到redis中 init Auth
 // 判断用户的权限，从redis中读取权限
-const RoleSchema = new Schema()
+// 用户的token保存了用户的角色信息, 修改用户的角色需要重新登录, 以重新获取角色
+// role和auth需要做关联的操作
+const RoleSchema = new Schema({
+  code: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  auths: {
+    // role表与auth表进行关联操作
+    // auths为角色的权限集合
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Auth'
+    }],
+    default: [],
+    required: true
+  }
+})
 
 module.exports = RoleSchema
