7c4845d7e05c3e732b62d72c14d2baae4133392f
BengBu-YueZhang <zhangyue_coder@163.com>
2018-08-29 16:34:46 +0800

    分页的bug

--- util/pagination.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (pagestart, pagesize) {
-  const start = ((pagestart - 1) * pagesize) - 1 < 0 ? 0 : ((pagestart - 1) * pagesize) - 1
-  const end = (pagestart * pagesize) - 1
-  return {
-    start,
-    end
-  }
-}
