2b0912858f47d2c3244a4fd0c9a8676c467089a4
BengBu-YueZhang <zhangyue_coder@163.com>
2018-08-29 21:09:45 +0800

    优化代码

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -4,6 +4,7 @@ const redisClient = require('../config/redis')
 const { promisify } = require('util')
 const getAsync = promisify(redisClient.get).bind(redisClient)
 const acl = require('./../config/acl').acl
+// const jwt = require('koa-jwt')
 
 /**
  * 登录权限控制
@@ -13,35 +14,34 @@ const acl = require('./../config/acl').acl
 module.exports = function (model, auth) {
   return async function (ctx, next) {
     // const token = ctx.headers['x-access-token']
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViODEzODAyZTgwNDhkMGRkNmE1MjE3MiIsInJvbGVzIjpbXSwiaWF0IjoxNTM1NTQyMjA2LCJleHAiOjE1MzU2Mjg2MDZ9.PmTc2lyE5OVUTTPPcMxXKLfoHGWHcAWga9NoifK4F9w'
+    // TODO: 方便测试展示写死token
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViODEzODAyZTgwNDhkMGRkNmE1MjE3MiIsInJvbGVzIjpbXSwiaWF0IjoxNTM1NTQ2Njc2LCJleHAiOjE1MzU2MzMwNzZ9.O-Rb8fdgAEeNWPxMGouMr2cig9kCec4Ftl_DulOKAnk'
     if (token) {
-      jwt.verify(token, secret, function (err, decoded) {
-        if (err) {
-          ctx.throw(403, 'token已过期')
-        } else {
-          // 使用redis进行登录的验证, 避免没有登录的时候, 使用之前的token进行访问
-          // 从token中获取id信息以及角色信息
-          const { id, roles } = decoded
-          getAsync(id).then(async (res) => {
-            if (!res) return ctx.throw(403, 'token失效')
-            ctx.decoded = decoded
-            if (!model || !auth) {
-              next()
-            } else {
-              // 接口权限验证
-              acl.areAnyRolesAllowed(roles, model, auth, function (err, result) {
-                if (result) {
-                  next()
-                } else {
-                  return ctx.throw(403, '没有操作权限')
-                }
-              })
-            }
-          }).catch(err => {
-            return ctx.throw(403, 'token失效')
-          })
+      // token验证
+      let decoded = null
+      try {
+        decoded = await jwt.verify(token, secret)
+      } catch (error) {
+        ctx.throw(403, 'token失效')
+      }
+      const { id, roles } = decoded
+      // 登录验证
+      try {
+        await getAsync(id)
+      } catch (error) {
+        ctx.throw(403, 'token已过期')
+      }
+      ctx.decoded = decoded
+      if (!model || !auth) {
+        await next()
+      } else {
+        try {
+          acl.areAnyRolesAllowed(roles, model, auth)
+        } catch (error) {
+          ctx.throw(403, '接口你没有权限')
         }
-      })
+        await next()
+      }
     } else {
       ctx.throw(403, '缺少token信息')
     }
